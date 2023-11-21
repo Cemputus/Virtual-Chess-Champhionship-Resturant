9b1773c79a5bab1c66f2155bf5bcab6cbc4da451
Cemputus <cemputus019@gmail.com>
2023-11-21 13:51:18 +0300

    Patra-and-Dave

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/css/style.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Products from "./components/Products";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Menu />
+      <Products />
+      <Review />
+      <Contact />
+      <Blog />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import "./assets/css/style.css";
+// import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Menu from "./components/Menu";
+// import Products from "./components/Products";
+// import Review from "./components/Review";
+// import Contact from "./components/Contact";
+// import Blog from "./components/Blog";
+// import Footer from "./components/Footer";
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Route path="/" exact component={Home} />
+//       <Route path="/about" component={About} />
+//       <Route path="/menu" component={Menu} />
+//       <Route path="/products" component={Products} />
+//       <Route path="/review" component={Review} />
+//       <Route path="/contact" component={Contact} />
+//       <Route path="/blog" component={Blog} />
+//       <Footer />
+//     </Router>
+//   );
+// };
+
+// export default App;
+
